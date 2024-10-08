568792b60b43651a2deba2aff97bc0c136238f49
Kathleen Garrity <kathleen.garrity223@gmail.com>
2024-10-08 12:30:02 -0400

    feat: prep for npm deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    // Dev config
+    return {
+      plugins: [react()],
+      root: path.resolve(__dirname, 'dev'),
+      build: {
+        outDir: path.resolve(__dirname, 'dist')
+      }
+    };
+  } else {
+    // Build config
+    return {
+      plugins: [react(), dts()],
+      build: {
+        lib: {
+          entry: path.resolve(__dirname, 'src/index.ts'),
+          name: 'AnimatedMenuIcon',
+          formats: ['es', 'umd'],
+          fileName: (format) => `animated-menu-icon.${format}.js`
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM'
+            }
+          }
+        }
+      }
+    };
+  }
+});
